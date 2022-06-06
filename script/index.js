89142c1fb50dbbc5bf19a56bf201c1c420e01b19
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-06-06 14:50:58 +0200

    Json

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
-const menu = JSON.parce([csvjson.json]);
-        
+let menu_json = JSON.parse(menus);
+let menus = "";
+
+
+for (let i=0; i < menu_json.length;i++){
+    document.getElementsByClassName(i);
+}
+
+
+
 
