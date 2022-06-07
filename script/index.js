aa0065a7e002b24993dcb066d74313ef122429eb
AndreaBern26 <101724840+AndreaBern26@users.noreply.github.com>
2022-06-07 12:26:44 +0200

    Corrección del html y css

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 let menu_json = JSON.parse(menus);
 let menus = "";
 
-
 for (let i=0; i < menu_json.length;i++){
-    document.getElementsByClassName(i);
+    menus += "<p>" + "Descripción: " + menu_json[i].descripcion + "<p>";
 }
 
+document.getElementsByClassName('lista_menu')[0].innerHTML = lista;
 
 
 
